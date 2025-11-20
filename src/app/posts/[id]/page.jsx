b7079fbe7505fce060e9af0c484e7cb752e71485
kris2tis <m.rezaei.coder@gmail.com>
2025-11-20 21:51:05 +0330

    ..

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -3,16 +3,21 @@ import axios from "axios";
 export default async function page({ params }) {
   const { id } = await params;
   const data = await axios
-    .post("https://learn-backend-for-frontend-9wdh.vercel.app/api/getPostById", { id })
+    .post(
+      "https://learn-backend-for-frontend-9wdh.vercel.app/api/getPostById",
+      { id }
+    )
     .then(({ data }) => data)
     .catch((err) => err);
 
-  if (!data) return <span>پستی با این آیدی یافت نشد</span>;
-  return (
+  return data?.title ? (
     <div className="flex flex-col gap-y-3 p-2 ">
       <h1>صفحه پست ها</h1>
+
       <span>{data.title}</span>
       <span className="text-justify">{data.content}</span>
     </div>
+  ) : (
+    <span>پستی یافت نشد</span>
   );
 }
