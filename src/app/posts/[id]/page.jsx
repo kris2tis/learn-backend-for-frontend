84cb57ccf3afa8d6f55b504e0e1d31144e6200a1
kris2tis <m.rezaei.coder@gmail.com>
2025-11-20 21:30:01 +0330

    add title for post page

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -10,6 +10,7 @@ export default async function page({ params }) {
   if (!data) return <span>پستی با این آیدی یافت نشد</span>;
   return (
     <div className="flex flex-col gap-y-3 p-2 ">
+      <h1>صفحه پست ها</h1>
       <span>{data.title}</span>
       <span className="text-justify">{data.content}</span>
     </div>
